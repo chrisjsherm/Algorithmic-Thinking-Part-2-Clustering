74ccb97258f49613720be8fecf91070a9f93e514
Chris Sherman <chris.sherman@outlook.com>
2018-08-20 09:04:37 -0400

    Stub out first clustering utility method and setup test suite.

---
+++ util_clustering.py
@@ -0,0 +1,16 @@
+"""
+Clustering utility for Rice University Algorithmic Thinking Part II: Project 3.
+"""
+
+def slow_closest_pair(cluster_list):
+  """
+  Takes a list of Cluster objects and returns a closest pair where the pair is
+  represented by a tuple (dist, idx1, idx2) with idx1 < idx2 where dist is the
+  distance between the closest pair cluster_list[idx1] and cluster_list[idx2].
+
+  @param list List of Cluster objects.
+
+  @return tuple (dist, idx1, idx2) Dist is the distance between the closest
+    pair in the cluster_list parameter cluster_list[idx1], cluster_list[idx2].
+  """
+  pass
---
+++ util_clustering.spec.py
@@ -0,0 +1,19 @@
+"""
+Test suite for the clustering utility.
+"""
+import unittest
+import util_clustering
+
+class TestUtilClustering(unittest.TestCase):
+  """
+  Unit tests for clustering utility.
+  """
+  def setUp(self):
+    """
+    Run before each test.
+    Each test method must begin with "test_".
+    """
+    pass
+
+  def test_slow_closest_pair(self):
+    cluster_list = []
