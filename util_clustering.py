4889aa0c9106aca203b50429f12475ab2b452067
Chris Sherman <chris.sherman@outlook.com>
2018-08-21 08:59:17 -0400

    Implement slow_closest_pair.

--- util_clustering.py
+++ util_clustering.py
@@ -1,16 +1,29 @@
 """
 Clustering utility for Rice University Algorithmic Thinking Part II: Project 3.
 """
+from alg_cluster import Cluster
+import math
+
 
 def slow_closest_pair(cluster_list):
-  """
-  Takes a list of Cluster objects and returns a closest pair where the pair is
-  represented by a tuple (dist, idx1, idx2) with idx1 < idx2 where dist is the
-  distance between the closest pair cluster_list[idx1] and cluster_list[idx2].
+    """
+    Takes a list of Cluster objects and returns a closest pair where the pair is
+    represented by a tuple (dist, idx1, idx2) with idx1 < idx2 where dist is the
+    distance between the closest pair cluster_list[idx1] and cluster_list[idx2].
+    This is a brute force algorithm that compares every cluster with every other
+    cluster.
 
-  @param list List of Cluster objects.
+    @param list List of Cluster objects.
 
-  @return tuple (dist, idx1, idx2) Dist is the distance between the closest
-    pair in the cluster_list parameter cluster_list[idx1], cluster_list[idx2].
-  """
-  pass
+    @return tuple (dist, idx1, idx2) Dist is the distance between the closest
+      pair in the cluster_list parameter cluster_list[idx1], cluster_list[idx2].
+    """
+    d_i_j = (float('inf'), -1, -1)
+    list_len = len(cluster_list)
+    for idx_i in range(list_len):
+        for idx_j in range(idx_i + 1, list_len):
+            dist = cluster_list[idx_i].distance(cluster_list[idx_j])
+            if dist < d_i_j[0]:
+                d_i_j = (dist, idx_i, idx_j)
+
+    return d_i_j
--- util_clustering.spec.py
+++ util_clustering.spec.py
@@ -3,17 +3,30 @@ Test suite for the clustering utility.
 """
 import unittest
 import util_clustering
+from alg_cluster import Cluster
+import util_clustering
+import math
+
 
 class TestUtilClustering(unittest.TestCase):
-  """
-  Unit tests for clustering utility.
-  """
-  def setUp(self):
     """
-    Run before each test.
-    Each test method must begin with "test_".
+    Unit tests for clustering utility.
     """
-    pass
 
-  def test_slow_closest_pair(self):
-    cluster_list = []
+    def setUp(self):
+        """
+        Run before each test.
+        Each test method must begin with "test_".
+        """
+        cluster1 = Cluster([32001, 32013, 32031], 2, 2, 400, .1)
+        cluster2 = Cluster([51121, 51155, 51161], 0, 0, 600, .2)
+        cluster3 = Cluster([51059, 51013, 51107], -2, -2, 800, .3)
+        self._cluster_list = [cluster1, cluster2, cluster3]
+
+    def test_slow_closest_pair(self):
+        self.assertEqual(util_clustering.slow_closest_pair(
+            self._cluster_list), (math.sqrt(8), 0, 1))
+
+test_suite = unittest.TestLoader().loadTestsFromTestCase(TestUtilClustering)
+unittest.TextTestRunner(verbosity=2).run(test_suite)
+# Run in terminal with: python ./util_clustering.spec.py
