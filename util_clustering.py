559fae0b6fb3661675b3ecea015bc157ae0ca00b
Chris Sherman <chris.sherman@outlook.com>
2018-08-23 08:58:00 -0400

    Implement merge sort.

--- util_clustering.py
+++ util_clustering.py
@@ -2,7 +2,6 @@
 Clustering utility for Rice University Algorithmic Thinking Part II: Project 3.
 """
 from alg_cluster import Cluster
-import math
 
 
 def slow_closest_pair(cluster_list):
@@ -27,3 +26,109 @@ def slow_closest_pair(cluster_list):
                 d_i_j = (dist, idx_i, idx_j)
 
     return d_i_j
+
+
+def closest_pair_strip(cluster_list, horiz_center, half_width):
+    """
+    Takes a list of Cluster objects and two floats, horiz_center and half_width.
+    Returns a tuple corresponding to the closest pair of clusters that lie in 
+    the specified strip (the pair of indices should be in ascending order).
+
+    @param list List of Cluster objects.
+    @param float Horizontal position of the center line for a vertical strip.
+    @param float Maximal distance of any point in the strip from the center line.
+
+    @return tuple (dist, idx1, idx2) Dist is the distance between the closest
+      pair of clusters in the specified strip.
+    """
+    # Build an array, strip, that contains points closer than half_width to,
+    # horiz_center, the line passing through the middle point.
+    strip = []
+    for idx_i in range(len(cluster_list)):
+        if(abs(cluster_list[idx_i].horiz_center() - horiz_center) < half_width):
+            strip.append(cluster_list[idx_i])
+
+    # Initialize the minimum distance.
+    min_distance = half_width
+
+    # Sort strip by y-coordinates.
+
+
+def fast_closest_pair(cluster_list):
+    """
+    Takes a list of Cluster objects and returns a closest pair where the pair is
+    represented by a tuple (dist, idx1, idx2) with idx1 < idx2 where dist is the
+    distance between the closest pair cluster_list[idx1] and cluster_list[idx2].
+    This is a divide and conquer algorithm.
+
+    @param list List of Cluster objects.
+
+    @return tuple (dist, idx1, idx2) Dist is the distance between the closest
+      pair in the cluster_list parameter cluster_list[idx1], cluster_list[idx2].
+    """
+    pass
+
+def merge_sort(arr, left, right):
+    """
+    Divide and conquer algorithm for sorting elements in a stable fashion.
+
+    @param array Elements to sort.
+    @param int First index of the left half of the array.
+    @param int Last index of the right half of the array.
+    """
+    if left < right:
+        middle = (left + right) // 2
+
+        # Sort first and second halves.
+        merge_sort(arr, left, middle)
+        merge_sort(arr, middle + 1, right)
+        merge(arr, left, middle, right)
+
+def merge(arr, left, middle, right):
+    """
+    Merge the two subarrays of arr. 
+    The first subarray is arr[left:middle].
+    The second subarray is arr[middle+1:right]
+
+    @param array Elements of the subarrays.
+    @param int First index of the left subarray.
+    @param int Middle index of the array.
+    @param int Last index of the right subarray.
+    """
+    len_left_arr = middle - left + 1
+    len_right_arr = right - middle
+
+    temp_left = [0] * len_left_arr
+    temp_right = [0] * len_right_arr
+
+    # Copy data into temporary arrays.
+    for idx_i in range(0, len_left_arr):
+        temp_left[idx_i] = arr[left + idx_i]
+    for idx_i in range(0, len_right_arr):
+        temp_right[idx_i] = arr[middle + 1 + idx_i]
+
+    idx_i = 0  # Initial index of the left subarray.
+    idx_j = 0  # Initial index of the right subarray.
+    idx_k = left  # Initial index of merged subarray.
+
+    # Merge the sorted temp arrays back into arr.
+    while idx_i < len_left_arr and idx_j < len_right_arr:
+        if temp_left[idx_i] <= temp_right[idx_j]:
+            arr[idx_k] = temp_left[idx_i]
+            idx_i += 1
+        else:
+            arr[idx_k] = temp_right[idx_j]
+            idx_j += 1
+        idx_k += 1
+
+    # Copy the remaining elements of temp_left, if any.
+    while idx_i < len_left_arr:
+        arr[idx_k] = temp_left[idx_i]
+        idx_i += 1
+        idx_k += 1
+
+    # Copy the remaining elements of temp_right, if any.
+    while idx_j < len_right_arr:
+        arr[idx_k] = temp_right[idx_j]
+        idx_j += 1
+        idx_k += 1
--- util_clustering.spec.py
+++ util_clustering.spec.py
@@ -27,6 +27,49 @@ class TestUtilClustering(unittest.TestCase):
         self.assertEqual(util_clustering.slow_closest_pair(
             self._cluster_list), (math.sqrt(8), 0, 1))
 
+    def test_closest_pair_strip(self):
+        self.assertEqual(util_clustering.closest_pair_strip(
+            self._cluster_list, 0, 1), (float('inf'), -1, -1))
+
+    def test_fast_closest_pair(self):
+        self.assertEqual(util_clustering.fast_closest_pair(
+            self._cluster_list), (math.sqrt(8), 0, 1))
+
+    def test_merge(self):
+        arr1 = [11, 12, 13, 5, 6, 7]
+        util_clustering.merge(arr1, 0, (len(arr1) - 1) // 2, len(arr1) - 1)
+        self.assertListEqual(arr1, [5, 6, 7, 11, 12, 13])
+
+        arr2 = [1, 3, 4, 7, 9]
+        util_clustering.merge(arr2, 0, (len(arr2) - 1) // 2, len(arr2) - 1)
+        self.assertListEqual(arr2, [1, 3, 4, 7, 9])
+
+        jordan = [23]
+        util_clustering.merge(jordan, 0, (len(jordan) - 1) // 2, len(jordan) - 1)
+        self.assertListEqual(jordan, [23])
+
+    def test_merge_sort(self):
+        arr1 = [12, 11, 13, 5, 6, 7]
+        util_clustering.merge_sort(arr1, 0, len(arr1) - 1)
+        self.assertListEqual(arr1, [5, 6, 7, 11, 12, 13])
+
+        arr2 = [4, 1, 3, 9, 7]
+        util_clustering.merge_sort(arr2, 0, len(arr2) - 1)
+        self.assertListEqual(arr2, [1, 3, 4, 7, 9])
+
+        arr3 = [10, 9, 8, 7, 6, 5, 4, 3, 2, 1]
+        util_clustering.merge_sort(arr3, 0, len(arr3) - 1)
+        self.assertListEqual(arr3, [1, 2, 3, 4, 5, 6, 7, 8, 9, 10])
+
+        arr4 = [54, 3, 44, 69, 101, 5, 18, 21]
+        util_clustering.merge_sort(arr4, 0, len(arr4) - 1)
+        self.assertListEqual(arr4, [3, 5, 18, 21, 44, 54, 69, 101])
+        
+        jordan = [23]
+        util_clustering.merge_sort(jordan, 0, len(jordan) - 1)
+        self.assertListEqual(jordan, [23])
+
+
 test_suite = unittest.TestLoader().loadTestsFromTestCase(TestUtilClustering)
 unittest.TextTestRunner(verbosity=2).run(test_suite)
 # Run in terminal with: python ./util_clustering.spec.py
